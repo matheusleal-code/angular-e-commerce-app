7b64bd1c34992c30d55d64f43a8f51cd9d35f8fb
Matheus Leal Toledo <matheus.leal@helpper.com.br>
2021-11-01 19:29:55 -0300

    add components and services

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
 })
-export class AppComponent {
+export class TopBarComponent {
+
 }
 
 
